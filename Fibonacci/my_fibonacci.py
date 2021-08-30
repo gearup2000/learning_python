8f0409b33929fc1bc3761e663d6088458b68809d
gearup2000 <antonywerr@gmail.com>
2021-08-30 12:22:56 +0300

    Added test module for testing my_fibonacci.py

--- Fibonacci/my_fibonacci.py
+++ Fibonacci/my_fibonacci.py
@@ -1,5 +1,27 @@
 def fib(n):
+    """
+    Function calculate the Fibonacci numbers from given number
+    >>> fib(1)
+    1
+    >>> fib(2)
+    1
+    >>> fib(3)
+    2
+    >>> fib(5)
+    5
+    >>> fib(10)
+    55
+    >>> fib(20)
+    6765
+    >>> fib(-1)
+    None
+    """
     if n < 2:
         return n
     else:
         return fib(n - 1) + fib(n - 2)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
